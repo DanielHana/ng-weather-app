08693611755b1c141b7d47202354b013992ba6c2
Code <code@COCTL-41268.local>
2019-06-06 10:59:45 -0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BtnComponent } from './shared/components/btn/btn.component';
+import { CityComponent } from './shared/components/city/city.component';
+import { HomeComponent } from './views/pages/home/home.component';
+import { SearchComponent } from './views/pages/search/search.component';
+import { ToastComponent } from './views/toast/toast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BtnComponent,
+    CityComponent,
+    HomeComponent,
+    SearchComponent,
+    ToastComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
